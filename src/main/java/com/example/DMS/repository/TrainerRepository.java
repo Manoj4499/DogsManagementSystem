48c56b12367af39c567711d16741c046243cb5ed
Manoj4499 <s561637@nwmissouri.edu>
2023-11-22 17:01:30 -0600

    project is added

---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,13 @@
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+
+/**
+ * @author Manoj Kumar Kandimalla
+ */
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
